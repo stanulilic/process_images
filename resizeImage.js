4a894a994efd531287a7820f1aeb217adbaf7179
Stanley Ulili <stanleyulili@gmail.com>
2021-09-08 11:57:51 +0200

    change image dimensions to 150x97 during resize operation

--- resizeImage.js
+++ resizeImage.js
@@ -4,9 +4,8 @@ async function resizeImage() {
   try {
     await sharp("sammy.png")
       .resize({
-        width: 100,
-        height: 100,
-        fit: "outside",
+        width: 150,
+        height: 97,
       })
       .toFormat("jpeg", { mozjpeg: true })
       .toFile("sammy-resized-compressed.jpeg");
